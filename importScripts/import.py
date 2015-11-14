e6abe63f1d366889c7d4414a92089eb77f3d8f58
Saad Kothawala <xssaad@yahoo.com>
2015-11-14 20:31:03 +0000

    added script to import courses:

---
+++ importScripts/import.py
@@ -0,0 +1,33 @@
+import openpyxl
+from string import ascii_uppercase
+import urllib
+
+wb = openpyxl.load_workbook('Course Enrollments.Fall 2015.10282015.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+#for row in range(2, sheet.get_highest_row() + 1):
+
+for row in range(2, 3):
+    # Each row in the spreadsheet has data for one census tract.
+    
+    
+    params = urllib.urlencode({
+        "subject" : sheet['A' + str(row)].value,
+        "career":sheet['B' + str(row)].value,
+        "catalog_number":sheet['I' + str(row)].value,
+        "section_number":sheet['J' + str(row)].value,
+        "course_title":sheet['L' + str(row)].value,
+        "intstructor":sheet['M' + str(row)].value ,
+        "intstructor_email":sheet['N' + str(row)].value,
+        "campus":sheet['X' + str(row)].value,
+        "building":sheet['Z' + str(row)].value,
+        "room_number":sheet['AA' + str(row)].value,
+        "meeting_patern":sheet['AC' + str(row)].value,
+        "start_time":sheet['AD' + str(row)].value,
+        "end_time":sheet['AE' + str(row)].value
+    })
+    
+    f = urllib.urlopen("https://classmatesconnect-skothawala.c9users.io/api/courses", params)
+    print f.read()
+
+    
+    
